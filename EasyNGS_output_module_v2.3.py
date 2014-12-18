293ae39b5016227ca871054678b6d42ad39f4b2e
sjsumbt <kiranmayee.dhavala@gmail.com>
2014-12-17 17:45:21 -0800

    Update and rename ngs_summary_v2_3.py to EasyNGS_output_module_v2.3.py

--- EasyNGS_output_module_v2.3.py
+++ EasyNGS_output_module_v2.3.py
@@ -21,9 +21,10 @@ to calculate insertions and deletions.
 import re
 import pandas as pd
 import wx     
-import EasyNGS_with_UI_v3_With_Comments as engs
+import EasyNGS_GUI_v3 as engs
 reload(engs)   
 import sys
+import EasyNGS_results_module_v2.1 as res
 
 app = wx.App()
 lb = engs.ListBox(None, -1, 'EasyNGS')
@@ -63,7 +64,6 @@ Modifcation history:
     
 """
 
-
 indels = 0 # insertions or deletions.
 
 def hasindel(element, regex=re.compile('\d+[ID]')):
@@ -74,7 +74,7 @@ def hasindel(element, regex=re.compile('\d+[ID]')):
     
     Postconditions: Returns either 'true' or 'false'
     
-    Side effects: none
+    Side effects: global variable "indels" used but no possible effect on the rest of the code
     
     Return: True if an indel is present, otherwise false.
     >>> hasindel('ABCDEI')
@@ -97,7 +97,7 @@ def hasindel(element, regex=re.compile('\d+[ID]')):
     else:
          return False
 
-index = 0
+index = 0 #initialize global variable "indels"
 indel_loc =[] #contains index of indels in CIGAR
 indel_span = [] #the span of the indels within CIGAR[index]
 indel_type = [] #list of strings containing the actual indel
@@ -117,74 +117,11 @@ for q in range(len(indel_loc)):
     total_indel_info.insert(q,((indel_loc[q], indel_type[q], indel_span[q]))) #merge the created lists into one list
 
 
-insertions = 0
-deletions = 0
-for i_or_d in indel_type:
-    if i_or_d[1] == "I":
-        insertions += int(i_or_d[0])
-    elif i_or_d[1] == "D":
-        deletions += int(i_or_d[0]) 
-        #calculates number of specific indels and deletionos
-        
-sam.set_cigar_total_indel_info(total_indel_info, insertions, deletions)
-print "Num insertions ",insertions
-print "Num deletions ", deletions
 
 """
 End of edit from ngs_summary_jeff by A.Subbarao 12/2/2014
 """
 
-#####deletions just returns the length of indel_type (doesn't seem to differentiate between I or D). 
-#I think indexing of loop is incorrect        
-
-
-#Calculate Indels for the file
-indelcount = 0
-for indels in mpileup.symbols:
-    if "-" in indels:
-        indelcount += 1
-    elif "+" in mpileup.symbols:
-        indelcount += 1
-print "Count of Insertions/Deletions = ",indelcount
-
-#Calculate SNPs for the file
-countofnucleotides = 0
-snpcount = 0
-substitutioncount = 0
-
-
-##count the number of A,C,T,G,a,c,t,g present in rbase
-acount = 0
-ccount = 0
-tcount = 0
-gcount = 0
-bases = ["A", "C", "T", "G", "a", "c", "t", "g"]
-for nucleotide in mpileup.rbase:
-    if nucleotide.lower() == 'a':
-        acount += 1 
-    elif nucleotide.lower() == 'c':
-        ccount += 1
-    elif nucleotide.lower() == 't':
-        tcount +=1
-    elif nucleotide.lower() == 'g':
-        gcount +=1
-
-print "Substitution count: "
-print "'a' count = {:d}, 'c' count {:d}, 't' count {:d}, 'g' count {:d}".format(acount, ccount, tcount, gcount)
-
-#####Looking at rbase we should see much larger numbers (all of them seem to be in bases), additionally we should expect count of nucleotides to be the same length as bases (that is: have a count per base)#
-#####snpcount will only be 1 if countofnecleotides is >= 1 and 0 otherwise (takes on no other values) (not sure if this in intentional)
-
-             
-#####Mapped_reads is missing some matches. I can't make sense of it logically, however when implementing my own version I found additional matches (which I verified)
-
-#Compute the coverage of each sequence
-haploidgenome_length = float(3*(10**9))##in basepairs;for human genome
-read_length = 65
-index = 0
-#for each in sam.read_seq:
-#    print "depth per base = ",mpileup.depth[index]
-#    index += 1
     
 #write the data to a csv file    
 data = [[design.ID, design.Name, design.refseq, design.Chromosome_Coordinates_designs, design.Mapping_quality_designs, sam.read_seq, sam.chromosome_coordinates ,design.Row, design.Column, design.Feature_number, mpileup.depth]]
@@ -192,7 +129,7 @@ data = [[design.ID, design.Name, design.refseq, design.Chromosome_Coordinates_de
 out = pd.DataFrame(data[0][0:10])
 fin = out.transpose()
 
-fin.to_csv('output_data.csv') ##instead of outputting the data I would use a key to how the data was subsetted
+fin.to_csv('output_data.csv') #writes the data given above to "output_data" in csv format in the working directory
 
 if __name__ == "__main__":
     import doctest
